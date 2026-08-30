ed1214edbe69c71b77b6bafc4d1ecbf915f0c51d
nourmuhammad632-maker <nourmuhammad632@gmail.com>
2026-08-30 06:32:36 +0300

    Question1

---
+++ ADV03/ClassHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADV03
+{
+    internal class ClassHelper
+    {
+        public static  void Printlist<T>(string name, List<T> list){
+        if(!string.IsNullOrEmpty(name)&& list is not null&&list.Count>0)
+            {
+                Console.WriteLine($"Name List:{name}");
+                Console.WriteLine(string.Join(", ", list));
+            }
+        }
+    }
+}
